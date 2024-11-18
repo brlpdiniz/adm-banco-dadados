4e4a8ecd086b5a80e904504095743654d77d51c9
brlpdiniz <brunoldiniz14@gmail.com>
2024-11-18 17:20:04 -0300

    feat: ed 2

---
+++ 2ºBimestre/EDs/2BI_TrabalhoFinal/IA_2.sql
@@ -0,0 +1,34 @@
+/*
+Vou situar e na próxima mensagem enviarei os comandos necessários:
+Nas reuniões, entre os gestores e a equipe de TI, para entendimento dos requisitos para análise e criação de um Data Mart, foram apresentadas as seguintes necessidades:
+i. Os gerentes da rede de restaurantes desejam acompanhar, através de um dashboard, o valor total das vendas diárias da empresa.
+ii. Eles identificaram que as visões necessárias seriam por:
+	a. Categoria dos pratos;
+	b. Data (dia da semana, dia, mês, bimestre, trimestre e ano)
+	c. Loja;
+	d. Estado;
+	e. Cidade;
+	f. Região;
+Obs: Considerar o Estado e Cidade da loja e não do cliente.
+*/
+
+
+
+/*
+1) Crie a base de dados conforme o modelo desenvolvido;
+
+2) Crie as ETLs, que atualizem o DM com os dados da base transacional restaurante. Lembre-se que deverá ter uma carga inicial com todos os dados e uma carga incremental que atualizará o DM de acordo com a data informada pelo usuário.
+*/
+
+
+
+/*
+3) Futuramente, terei de criar no Power BI um dashboard para disponibilizar as seguintes informações:
+	a. Total vendido por cidade;
+	b. Mostrar o Top 3 das lojas com mais vendas;
+	c. Mostrar o valor total vendido por categoria do prato;
+	d. Mostrar o Top 10 das categorias de prato mais vendidas
+	e. Valor total vendido por mês;
+O dashboard deverá possibilitar os filtros para refinamento das informações. Esses filtros seriam:
+	período, semestre, Loja, Categoria do prato, estado e cidade
+*/
